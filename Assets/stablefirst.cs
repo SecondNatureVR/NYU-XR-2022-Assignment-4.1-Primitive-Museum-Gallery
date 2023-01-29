ea9d76a703db4127829741772de26d14bbc1b1ca
Johnny <secondnaturevr@gmail.com>
2023-01-29 12:03:53 -0500

    rube goldberg

--- Assets/stablefirst.cs
+++ Assets/stablefirst.cs
@@ -6,17 +6,11 @@ public class StableFirst : MonoBehaviour
 {
     [SerializeField] public float threshold = 50f;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         foreach(var rb in GetComponentsInChildren<Rigidbody>()) {
             rb.sleepThreshold = threshold;
             rb.Sleep();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
